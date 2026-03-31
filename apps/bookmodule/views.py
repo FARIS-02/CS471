568d1c359d422d10e838ca9412f901cd3499ada1
FARIS-02 <fij14244@gmail.com>
2026-03-31 11:16:39 +0300

    lab2

---
+++ apps/bookmodule/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    name = request.GET.get("name") or "world!" 
+    return render(request, "bookmodule/index.html") 
+def index2(request, val1 = 0): 
+    return HttpResponse("value1 = "+str(val1))
